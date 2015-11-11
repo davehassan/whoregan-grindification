e10bb081794e29566e7d1fd7cf253dade2159e8c
JimmyDavid <JimmyDavid>
2015-11-11 11:29:25 -0500

    Add note.js and tones.js

---
+++ FluxOrganApp/app/assets/javascripts/util/note.js
@@ -0,0 +1,32 @@
+var AudioContext = window.AudioContext || window.webkitAudioContext;
+var ctx = new AudioContext();
+
+var Note = window.Note = function (freq) {
+
+  var createOscillator = function (frequency) {
+    var osc = ctx.createOscillator();
+    osc.type = "sine";
+    osc.frequency.value = frequency;
+    osc.detune.value = 0;
+    osc.start(ctx.currentTime);
+    return osc;
+};
+
+  var createGainNode = function () {
+    var gainNode = ctx.createGain();
+    gainNode.gain.value = 0;
+    gainNode.connect(ctx.destination);
+    return gainNode;
+  };
+
+  this.oscillatorNode = createOscillator(freq);
+  this.gainNode = createGainNode();
+  this.oscillatorNode.connect(this.gainNode);
+};
+
+Note.prototype.start = function () {
+  this.gainNode.gain.value = 0.3;
+};
+Note.prototype.stop = function () {
+  this.gainNode.gain.value = 0;
+};
